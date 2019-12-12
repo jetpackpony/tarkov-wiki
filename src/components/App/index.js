5b143fa264822beb2b6a4e830cb25dba3cdd328e
jetpackpony <jetpackpony@gmail.com>
2019-12-12 16:27:36 +0300

    Add search

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,57 @@
 import { h } from 'preact';
+import { Fragment, useState } from 'preact/compat';
+import _ from 'lodash';
+import { search, getPage } from '../../wikiAPI';
+
+const searchDelay = 500;
+
+const searchTerm = _.debounce(
+  (term, onResult) => {
+    search(term)
+      .then(onResult);
+  },
+  searchDelay
+);
+const onInputChange = (e, onResult) => {
+  const val = e.target.value;
+  if (val.length >= 3) {
+    searchTerm(val, onResult);
+  } else {
+    searchTerm.cancel();
+  }
+};
 
 const App = () => {
-  const onInputChange = (e) => {
-    console.log(e);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchResults, setSearchRes] = useState([]);
+  const onResult = (res) => {
+    setIsLoading(false);
+    setSearchRes(res);
+  };
+  const onInput = (e) => {
+    setIsLoading(true);
+    onInputChange(e, onResult);
   };
   return (
-    <input type="text" onChange={onInputChange} />
+    <>
+      <input type="text" onInput={onInput} />
+      <div id="search-results">
+        {
+          (isLoading)
+            ? <div>Loading...</div>
+            : (
+              <ul>
+                {
+                  searchResults.map((res) => (
+                    <li onClick={() => console.log("click", res)}>{res}</li>
+                  ))
+                }
+              </ul>
+            )
+        }
+      </div>
+      <div id="item-details"></div>
+    </>
   );
 };
 
