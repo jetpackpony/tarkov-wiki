ed5972a3750aafcfbb4f271f068b9d25636cd1e0
jetpackpony <jetpackpony@gmail.com>
2019-12-12 19:09:14 +0300

    Add basic page loading/output

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,15 +2,46 @@ import { h } from 'preact';
 import { Fragment, useState } from 'preact/compat';
 import _ from 'lodash';
 import SearchBox from '../SearchBox';
+import { getPage } from '../../wikiAPI';
 
 const App = () => {
-  const onPageSelected = (page) => {
-    console.log("Page selected: ", page);
+  const [pageData, setPageData] = useState(null);
+  const onPageSelected = async (pageTitle) => {
+    console.log("Page selected: ", pageTitle);
+    const pageContent = await getPage(pageTitle);
+    console.log("Page loded: ", pageContent);
+    setPageData({
+      title: pageTitle,
+      price: pageContent.info.price,
+      img: pageContent.mainImage,
+      url: pageContent.url,
+      content: pageContent.content
+    })
   };
   return (
     <>
       <SearchBox onPageSelected={onPageSelected} />
-      <div id="item-details"></div>
+      <div id="item-details">
+        {
+          (pageData)
+            ? (
+              <>
+                <h1><a href={pageData.url}>{pageData.title}</a></h1>
+                <img src={pageData.img} />
+                <div>Price: {pageData.price}</div>
+                {
+                  pageData.content.map((section) => (
+                    <>
+                      <h2>{section.title}</h2>
+                      <p>{section.content}</p>
+                    </>
+                  ))
+                }
+              </>
+            )
+            : null
+        }
+      </div>
     </>
   );
 };
